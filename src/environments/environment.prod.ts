3c0f302639d8d1947aedd7a2fbc82b33ebad853c
KonradFeher <konradfeher@outlook.com>
2023-05-07 15:14:15 +0200

    env var upload (coospacen lemaradt :( )

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   firebase: {
-    /* TODO: put your environment here */
+    apiKey: "AIzaSyA9yZJdn2GpbeEW1_KTXFrEMbzbl_h7eUc",
+    authDomain: "webkert-c649q1.firebaseapp.com",
+    projectId: "webkert-c649q1",
+    storageBucket: "webkert-c649q1.appspot.com",
+    messagingSenderId: "1034155227039",
+    appId: "1:1034155227039:web:c8ae17b087bede4418afaa"
   },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,12 @@
 
 export const environment = {
   firebase: {
-    // TODO: CooSpacen be van adva.
+    apiKey: "AIzaSyA9yZJdn2GpbeEW1_KTXFrEMbzbl_h7eUc",
+    authDomain: "webkert-c649q1.firebaseapp.com",
+    projectId: "webkert-c649q1",
+    storageBucket: "webkert-c649q1.appspot.com",
+    messagingSenderId: "1034155227039",
+    appId: "1:1034155227039:web:c8ae17b087bede4418afaa"
   },
   production: false,
   hostUrl: 'http://localhost:4200'
